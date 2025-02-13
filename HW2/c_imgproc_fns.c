5038a4bc2b3ccc40bff01f4dc5303e19fe840644
Brendan Meixner 600.229 Sp25 DH <bmeixne1@ugradx.cs.jhu.edu>
2025-02-12 19:20:57 -0500

    kaleidoscope works

--- HW2/c_imgproc_fns.c
+++ HW2/c_imgproc_fns.c
@@ -216,8 +216,27 @@ int imgproc_kaleidoscope( struct Image *input_img, struct Image *output_img ) {
   if (input_height != input_width) {
     return 0;
   }
+  int32_t wedge_length = input_width/2;
+  int32_t l = input_height - 1;
 
   // TODO
+  for (int32_t y = 0; y < wedge_length; y++) {
+    for (int32_t x = y; x < wedge_length; x++) {
+      uint32_t input_pixel = input_img->data[compute_index(input_img, x, y)];
+
+      uint32_t output_pixel = input_pixel;
+      output_img->data[compute_index(output_img, x, y)] = output_pixel;
+      output_img->data[compute_index(output_img, l - x, y)] = output_pixel;
+      output_img->data[compute_index(output_img, l - y, x)] = output_pixel;
+      output_img->data[compute_index(output_img, l - y, l - x)] = output_pixel;
+
+      output_img->data[compute_index(output_img, y, x)] = output_pixel;
+      output_img->data[compute_index(output_img, y, l - x)] = output_pixel;
+      output_img->data[compute_index(output_img, x, l - y)] = output_pixel;
+      output_img->data[compute_index(output_img, l - x, l - y)] = output_pixel;
+      
+    }
+  }
 
   int32_t wedge_length = input_width/2;
   int32_t l = input_height - 1;
