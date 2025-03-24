e660955f5cd58d99de784e45615ce4b8d1a02167
Cooper Berube <cberube3@jh.edu>
2025-03-23 21:23:34 -0400

    .cpp function defintions

--- HW3/cacheSimFuncs.cpp
+++ HW3/cacheSimFuncs.cpp
@@ -1,2 +1,36 @@
 #include "cacheSimFuncs.h"
 
+Cache::Cache(char* args[]) {
+    setCount = std::stoi(args[1]);
+    blockCount = std::stoi(args[2]);
+    isWriteAllocate = (std::string(args[3]) == "write-allocate");
+    isWriteThrough = (std::string(args[4]) == "write-through"); // if false, then it's write-back
+    isLRU = (std::string(args[5]) == "lru");
+
+    // Initialize cache sets
+    sets.resize(setCount);
+}
+
+unsigned int Cache::getTag(std::uint32_t address, unsigned byteOffsetBits, unsigned indexBits) {
+    return address >> (byteOffsetBits + indexBits);
+}
+
+unsigned int Cache::getIndex(std::uint32_t address, unsigned byteOffsetBits) {
+    return (address >> byteOffsetBits) & (setCount - 1);
+}
+
+void Cache::loadAddress(std::uint32_t address, unsigned byteOffsetBits, unsigned indexBits) {
+    // Implement load operation
+}
+
+void Cache::storeAddress(std::uint32_t address, unsigned byteOffsetBits, unsigned indexBits) {
+    // Implement store operation
+}
+
+void Cache::evict(unsigned int index, unsigned int tag) {
+    // Implement LRU eviction policy
+}
+
+void Cache::addBlock(unsigned int index, unsigned int tag) {
+    // Implement adding a cache slot
+}
