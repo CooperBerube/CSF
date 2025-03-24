28dd263f25ffec78f741d086a9d1ce8ec1e7b4b8
Brendan Meixner 600.229 Sp25 DH <bmeixne1@ugradx.cs.jhu.edu>
2025-03-23 20:42:52 -0400

    added classes to .h

--- HW3/cacheSimFuncs.h
+++ HW3/cacheSimFuncs.h
@@ -5,5 +5,17 @@
 #include <vector>
 #include <string>
 #include <map>
+class slot {
+    unsigned int tag;
+    bool valid, dirty;
+    unsigned int load_ts, access_ts;
+};
 
+class set {
+    std::vector<set> slots;
+};
+
+class cache {
+    std::vector<set> sets;
+};
 #endif // CACHESIMFUNCS_H
