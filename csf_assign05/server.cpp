28e8766f40c7fb306f1b2acd74933ded4ab5722d
Brendan Meixner 600.229 Sp25 DH <bmeixne1@ugradx.cs.jhu.edu>
2025-04-27 16:34:51 -0400

    continued progess on server

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -20,6 +20,7 @@ struct ConnInfo {
   Connection* connection;
   const int* port;
   int clientfd;
+  Server* server;
 };
 
 
@@ -41,29 +42,95 @@ void *worker(void *arg) {
   //       to communicate with a client (sender or receiver)
   ConnInfo *aux = static_cast<ConnInfo*>(arg); 
   Message received;
-  Message* toSend;
+  Message toSend;
   Connection* conn = aux->connection;
   std::string username;
   
- 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
-  
+
   conn->receive(received);
-  User currUser(received.data);
-  if (received.tag == TAG_RLOGIN) {
-    while(1) {
-      
+  User* currUser;
+  if (received.tag == TAG_RLOGIN || received.tag == TAG_SLOGIN) {
+    toSend.tag = TAG_OK;
+    toSend.data = "received";
+    if (received.data.length() == 0) {
+      toSend.tag = TAG_ERR;
+      toSend.data = "no username";
+    } else {
+    currUser = new User(received.data);
     }
-  } else if (received.tag == TAG_SLOGIN) {
-
+  } else {
+    toSend.tag = TAG_ERR;
+    toSend.data = "error";
   }
-
+  conn->send(toSend);
+  
   // TODO: depending on whether the client logged in as a sender or
   //       receiver, communicate with the client (implementing
   //       separate helper functions for each of these possibilities
   //       is a good idea)
+  std::string userType = received.tag;
+  Room* room = nullptr;
+  if (userType == TAG_RLOGIN) {
+    conn->receive(received);
+    if (received.tag != TAG_JOIN) {
+      toSend.tag = TAG_ERR;
+      toSend.data = "not join commmand";
+    } else if (received.data.length() == 0) {
+      toSend.tag = TAG_ERR;
+      toSend.data = "no romm name";
+    } else {
+      toSend.tag = TAG_OK;
+      toSend.data = "received";
+      room = (aux->server)->find_or_create_room(received.data); //come back to this
+    }
+    
+    conn->send(toSend);
+    while (1) {
+      conn->receive(received);
+      //add a delivery system from room to receiver
+    }
 
+  } else if (userType == TAG_SLOGIN) {
+    while (1) {
+      conn->receive(received);
+      if (received.tag == TAG_JOIN) {
+        if (received.data.length() == 0) {
+          toSend.tag == TAG_ERR;
+          toSend.data == "no room name";
+        } else {
+          toSend.tag == TAG_OK;
+          toSend.data == "received";
+          room = (aux->server)->find_or_create_room(received.data);
+          room->add_member(currUser);
+        }
+      } else if (received.tag == TAG_LEAVE) {
+        if (room == nullptr) {
+          toSend.tag = TAG_ERR;
+          toSend.data = "not in a room";
+        } else {
+          room->remove_member(currUser);
+          room = nullptr;
+        }
+
+      } else if (received.tag == TAG_SENDALL) {
+        if (room == nullptr) {
+          toSend.tag = TAG_ERR;
+          toSend.data = "not in a room";
+        } else if (received.data.length() == 0) {
+          toSend.tag = TAG_ERR;
+          toSend.data = "no message to send";
+        } else {
+          room->broadcast_message(currUser->username,received.data);
+        }
+      } else {
+        toSend.tag = TAG_ERR;
+        toSend.data = "unknown tag";
+      }
+      conn->send(toSend);
+    }
+  }
   
 
   close(aux->clientfd);
@@ -108,19 +175,17 @@ bool Server::listen() {
 void Server::handle_client_requests() {
   // TODO: infinite loop calling accept or Accept, starting a new
   //       pthread for each connected client
-
   while(1) {
     int clientfd = accept(m_ssock, nullptr, nullptr);
     if (clientfd < 0) {
-      // Error handling here
+      std::cerr << "Error accepting client" << std::endl;
     }
     struct ConnInfo *aux = static_cast<struct ConnInfo *>(malloc(sizeof(struct ConnInfo)));
     Connection conn(clientfd);
     aux->connection = &conn;
     aux->port = &m_port;
     aux->clientfd = clientfd;
-    
-
+    aux->server = this;
     pthread_t curr_thread;
     if(pthread_create(&curr_thread, nullptr, worker, aux) != 0) {
       std::cerr << "Error pthread problem" << std::endl; //fix
